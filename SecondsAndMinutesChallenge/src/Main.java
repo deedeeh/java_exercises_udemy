a1677ca333d664913665cd176d11f2a02e9dc332
deedeeh <dinahafez.8@gmail.com>
2020-01-04 18:08:12 +0000

    Added bonus by adding a 0 if h, m or s is less than 10

--- SecondsAndMinutesChallenge/src/Main.java
+++ SecondsAndMinutesChallenge/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(SecondsAndMinutes.getDurationString(65, 45));
-        System.out.println(SecondsAndMinutes.getDurationString(3945));
+        System.out.println(SecondsAndMinutes.getDurationString(150, 56));
+        System.out.println(SecondsAndMinutes.getDurationString(2450));
     }
 }
--- SecondsAndMinutesChallenge/src/SecondsAndMinutes.java
+++ SecondsAndMinutesChallenge/src/SecondsAndMinutes.java
@@ -1,17 +1,29 @@
 public class SecondsAndMinutes {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
     public static String getDurationString(int minutes, int seconds) {
         if(minutes < 0 || seconds < 0 || seconds > 59) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
         int calcHours = minutes / 60;
         int calcMinutes = minutes % 60;
-        return calcHours + "h " + calcMinutes + "m " + seconds + "s";
-
+        String hoursString = calcHours + "h";
+        String minutesString = calcMinutes + "m";
+        String secondsString = seconds + "s";
+        if(calcHours < 10) {
+            hoursString = "0" + hoursString;
+        }
+        if (calcMinutes < 10) {
+            minutesString = "0" + minutesString;
+        }
+        if (seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+        return hoursString + " " + minutesString + " " + secondsString;
     }
 
     public static String getDurationString(int seconds) {
         if(seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
         int calcMinutes = seconds / 60;
         int calcSeconds = seconds % 60;
